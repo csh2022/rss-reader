96a9e6b0090e811533f231a7528cecdffa701142
chenshanhui <1065521866@qq.com>
2025-03-06 02:36:29 +0800

    fix: fix docker

--- hack/run_docker.sh
+++ hack/run_docker.sh
@@ -5,4 +5,4 @@ set -ex
 docker stop rss-reader || true
 docker container rm rss-reader || true
 
-docker run -d -p 3001:3001 --name rss-reader rss-reader-runtime:latest
+docker run -d -p 3001:3001 -p 5050:5050 --name rss-reader rss-reader-runtime:latest
